fd41f13306f450693c1a725a110a02c4de0fe779
david <davidvillegas@effit.com.mx>
2021-09-23 11:16:36 -0500

    login y registro
    se crea login y registro

--- TutorialFireBase/AuthViewController.swift
+++ TutorialFireBase/AuthViewController.swift
@@ -25,9 +25,31 @@ class AuthViewController: UIViewController {
     
     
     @IBAction func signInButtonAction(_ sender: Any) {
+        if let email = emailTextFiel.text, let password = passworTextFiel.text {
+            Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                if let result = result, error == nil{
+                    self.navigationController?.pushViewController(HomeViewController(email: result.user.email!, provider: .basic), animated: true)
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error al registrar al usuario", preferredStyle: UIAlertController.Style.alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     @IBAction func logInActionButton(_ sender: Any) {
+        if let email = emailTextFiel.text, let password = passworTextFiel.text {
+            Auth.auth().signIn(withEmail: email, password: password) { result, error in
+                if let result = result, error == nil{
+                    self.navigationController?.pushViewController(HomeViewController(email: result.user.email!, provider: .basic), animated: true)
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error al registrar al usuario", preferredStyle: UIAlertController.Style.alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
     }
     
 }
